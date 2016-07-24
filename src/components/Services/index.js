b49c0deca28032124623615295c0ea77f8a62730
Peter Tait <contact@petertait.com>
2016-07-24 11:39:23 +0100

    fix services alignment

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -7,22 +7,24 @@ import { breakpoint, layout, type } from '../../vars'
 class Services extends Component {
   render() {
     return (
-      <section className='container' style={styles.container}>
-        <h3>Services</h3>
-        <div style={styles.services}>
-          <Service
-            image='images/eye.svg'
-            title='Print, branding &amp; graphic design'
-            summary='Modern design mixed with the theory of traditional graphic design.' />
-          <Service
-            nthChild='odd'
-            image='images/design.svg'
-            title='Application &amp; website design'
-            summary='User-centered website and application design. Using Agile methods to produce quick and effective results.' />
-          <Service
-            image='images/code.svg'
-            title='Front-end development'
-            summary='The entire process from rapid prototyping, visual concepts to a fully functional live product.' />
+      <section style={styles.container}>
+        <div className='container'>
+          <h3>Services</h3>
+          <div style={styles.services}>
+            <Service
+              image='images/eye.svg'
+              title='Print, branding &amp; graphic design'
+              summary='Modern design mixed with the theory of traditional graphic design.' />
+            <Service
+              nthChild='odd'
+              image='images/design.svg'
+              title='Application &amp; website design'
+              summary='User-centered website and application design. Using Agile methods to produce quick and effective results.' />
+            <Service
+              image='images/code.svg'
+              title='Front-end development'
+              summary='The entire process from rapid prototyping, visual concepts to a fully functional live product.' />
+          </div>
         </div>
       </section>
     )
